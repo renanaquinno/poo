b31473a3bf93e1b98095839cf8e1a3ee30474c46
Erick Amorim <erickamorimcarv@gmail.com>
2022-11-29 21:13:45 -0300

    Class TesouroDireto

---
+++ Trabalho_Final/TesouroDireto.ts
@@ -0,0 +1,11 @@
+class TesouroDireto extends Investimento {
+    private _vencimento: string;
+    private _taxaRetorno: string;
+
+    constructor(id: string, nome: string, valor: number, vencimento: string, taxaRetorno: string) {
+        super(id, nome, valor);
+        this._vencimento = vencimento;
+        this._taxaRetorno = taxaRetorno;
+    }
+
+}
