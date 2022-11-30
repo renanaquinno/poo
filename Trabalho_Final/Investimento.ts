b2da133ddd5d47c5fbca59bb4e696b9838a62f30
Erick Amorim <erickamorimcarv@gmail.com>
2022-11-29 21:12:20 -0300

    Classe Investimento

---
+++ Trabalho_Final/Investimento.ts
@@ -0,0 +1,11 @@
+class Investimento {
+    private _id: string;
+    private _nome: string;
+    private _valor: number;
+
+    constructor(id:string, nome: string, valor: number) {
+        this._id = id;
+        this._nome = nome;
+        this._valor = valor;
+    }
+}
