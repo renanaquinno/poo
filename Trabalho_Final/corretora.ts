889714ae4dcdd8688a53f161f95c13d1570c9ffc
Erick Amorim <erickamorimcarv@gmail.com>
2022-12-05 12:01:54 -0300

    ajustes - erick

--- Trabalho_Final/corretora.ts
+++ Trabalho_Final/corretora.ts
@@ -1,6 +1,6 @@
 class Corretora {
     private _homeBroker: Acao[] = [];
-    private _contas: Conta[] = [];
+//    private _contas: Conta[] = [];
 
     adicionarAcao(acao: Acao) {
         try {
@@ -14,48 +14,16 @@ class Corretora {
         }
     }
 
-    comprarAcao(id_conta : string, acao: [string, string, number]){
-        let indice = this.consultarIndiceConta(id_conta);
-        let valor_acao = this.consultarValorAcao(acao[0]);
-        
-        let qtd_acoes = (parseFloat(acao[1]) / valor_acao.valor);        
-        acao[2] = (qtd_acoes);
-        if (indice != -1) {
-            this._contas[indice].comprarAcao(acao);
-        }
+    editarNomeAcao(id: string, novoNome: string): void {
+        this.consultarAcao(id).nome = novoNome;
     }
 
-    venderAcao(id_conta : string, acao: [string, string,number]){
-        let indice: number = this.consultarIndiceConta(id_conta);
-        if (indice != -1) {
-            for (var i = indice; i < this._contas.length; i++) {
-                this._contas[i] = this._contas[i + 1];
-            }
-            this._contas.pop();
-            this._contas[indice].venderAcao(acao);
-        }
-    }
-    
-    consultarIndiceConta(id_conta: string): number {
-        let indiceProcurado: number = -1;
-        for (let i = 0; i < this._contas.length; i++) {
-            if (this._contas[i].id == id_conta) {
-                indiceProcurado = i;
-            }
-        }
-        return indiceProcurado;
+    editarPrecoAcao(id: string, novoPreco: number): void {
+        this.consultarAcao(id).valor = novoPreco;
     }
 
-       
-    consultarConta(id_conta: string): Conta {
-        let contaProcurada!: Conta;
-
-        for (let i = 0; i < this._contas.length; i++) {
-            if (this._contas[i].id == id_conta) {
-                contaProcurada = this._contas[i];
-            }
-        }
-        return contaProcurada;
+    editarTicketAcao(id: string, novoTicket: string): void {
+        this.consultarAcao(id).ticket = novoTicket;
     }
 
     consultarAcao(id: string): Acao {
@@ -84,10 +52,54 @@ class Corretora {
         }
         return acaoProcurada;
     }
+
+    // comprarAcao(id_conta : string, acao: [string, string, number]){
+    //     let indice = this.consultarIndiceConta(id_conta);
+    //     let valor_acao = this.consultarValorAcao(acao[0]);
+        
+    //     let qtd_acoes = (parseFloat(acao[1]) / valor_acao.valor);        
+    //     acao[2] = (qtd_acoes);
+    //     if (indice != -1) {
+    //         this._contas[indice].comprarAcao(acao);
+    //     }
+    // }
+
+    // venderAcao(id_conta : string, acao: [string, string,number]){
+    //     let indice: number = this.consultarIndiceConta(id_conta);
+    //     if (indice != -1) {
+    //         for (var i = indice; i < this._contas.length; i++) {
+    //             this._contas[i] = this._contas[i + 1];
+    //         }
+    //         this._contas.pop();
+    //         this._contas[indice].venderAcao(acao);
+    //     }
+    // }
+    
+    // consultarIndiceConta(id_conta: string): number {
+    //     let indiceProcurado: number = -1;
+    //     for (let i = 0; i < this._contas.length; i++) {
+    //         if (this._contas[i].id == id_conta) {
+    //             indiceProcurado = i;
+    //         }
+    //     }
+    //     return indiceProcurado;
+    // }
+
+       
+    // consultarConta(id_conta: string): Conta {
+    //     let contaProcurada!: Conta;
+
+    //     for (let i = 0; i < this._contas.length; i++) {
+    //         if (this._contas[i].id == id_conta) {
+    //             contaProcurada = this._contas[i];
+    //         }
+    //     }
+    //     return contaProcurada;
+    // }
+
 }
 
-export {Corretora};
-
-// interface Transacao {
-//     operacao(id_acionista: number,cod_acao: string, qtd: number, operacao: number): void;
-// }
+let a01: Acao = new Acao('1', 'Itaú', 19.90, 'ITUB4');
+let corretora: Corretora = new Corretora();
+corretora.adicionarAcao(a01);
+console.log(corretora.consultarAcao('1'));
